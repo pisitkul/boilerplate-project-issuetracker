88751ad86680a21e3b704d3c6b3071a0bdf7f3c8
Shitdogg <pratchaya.ueapisitkul@gmail.com>
2025-05-08 12:47:52 +0700

    ีtest: test frist time

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,30 @@
-'use strict';
+"use strict";
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
+      res.json({
+        project: project,
+        issue_title: req.query.issue_title,
+        issue_text: req.query.issue_text,
+        created_by: req.query.created_by,
+        assigned_to: req.query.assigned_to,
+        status_text: req.query.status_text,
+      });
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,39 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite("Functional Tests", function () {
+  suite(
+    "POST /api/issues/{project} => create issue object/expect issue object",
+    function () {
+      test("Every field filled in", function (done) {
+        chai
+          .request(server)
+          .post("/api/issues/test")
+          .send({
+            issue_title: "Title",
+            issue_text: "Text",
+            created_by: "User",
+            assigned_to: "Assignee",
+            status_text: "In Progress",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body);
+            assert.property(res.body, "_id");
+            assert.property(res.body, "issue_title");
+            assert.property(res.body, "issue_text");
+            assert.property(res.body, "created_on");
+            assert.property(res.body, "updated_on");
+            assert.property(res.body, "created_by");
+            assert.property(res.body, "assigned_to");
+            assert.property(res.body, "status_text");
+            done();
+          });
+      });
+    }
+  );
 });
